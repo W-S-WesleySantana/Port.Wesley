e5c5faebe0419011484d60cdadc2a4f840859c81
wesley santana <wesleymemo7@gmail.com>
2026-07-17 16:43:11 -0300

    correçao 17

---
+++ js/certificados.js
@@ -0,0 +1,70 @@
+const projetos = [
+    {
+        nome: "HTML",
+        imagem: "assets/imagens/Certificados/Certificado - HTML - Front End Club.png",
+
+    },
+    {
+        nome: "INGLES",
+        imagem: "assets/imagens/Certificados/CERTIFICADO 1 inglês .jpg",
+
+    },
+    {
+        nome: "EXCEL",
+        imagem: "assets/imagens/Certificados/Excel-na-Prática.jpg",
+
+    },
+    {
+        nome: "HARDWARE SOFTWARE",
+        imagem: "assets/imagens/Certificados/Fundamentos-de-TI-Hardware-e-Software (1).jpg",
+
+    },
+    {
+        nome: "LGPD",
+        imagem: "assets/imagens/Certificados/Lei-Geral-de-Proteção-de-Dados-_LGPD_.jpg",
+
+    },
+    {
+        nome: "SEG. DA TECNOLOGIA DA INFORMAÇÃO",
+        imagem: "assets/imagens/Certificados/Segurança-em-Tecnologia-da-Informação.jpg",
+
+    },
+
+]
+
+
+const listaHTML = document.querySelector(".caixa-certificados");
+  
+  
+  function renderizarProdutos() {
+    listaHTML.innerHTML = ""; 
+  
+    
+    projetos.forEach(item => { 
+      
+      
+      const li = document.createElement("li"); 
+      
+     
+      li.classList.add("projeto"); 
+      li.classList.add("card-produto");
+  
+      li.innerHTML = `
+        
+        <ul class="caixa-certificados">
+
+            <li class="certificados">
+
+        <img class="img-certificado" src="${item.imagem}">
+
+             </li>
+
+        </ul>
+      `;
+  
+      listaHTML.appendChild(li);
+    });
+  }
+  
+  // 4. Executa a renderização
+  renderizarProdutos();
--- js/script.js
+++ js/script.js
@@ -5,26 +5,22 @@ const projetos = [
       nome: "Gw-Eletronico",
       modelo: "E-commerce Vanilla",
       imagem: "assets/imagens/logo projetos/Logo gw.jpg",
-      descricao: "GW Eletrônico é uma aplicação web moderna e responsiva voltada para o comércio ou gerenciamento de produtos eletrônicos. Desenvolvido inteiramente com tecnologias web fundamentais (HTML, CSS e JavaScript), o projeto destaca-se pela interface intuitiva, carregamento rápido e foco na experiência do usuário (UX)."
+      descricao: "GW Eletrônico é uma aplicação web de comércio e gestão de eletrônicos feita com HTML, CSS e JavaScript. O projeto destaca-se pela interface intuitiva, carregamento rápido e foco na experiência do usuário."
     },
     {
-      nome: "Gw-Eletronico",
-      modelo: "E-commerce Vanilla",
-      imagem: "assets/imagens/logo projetos/Logo gw.jpg",
-      descricao: "GW Eletrônico é uma aplicação web moderna e responsiva voltada para o comércio ou gerenciamento de produtos eletrônicos. Desenvolvido inteiramente com tecnologias web fundamentais (HTML, CSS e JavaScript), o projeto destaca-se pela interface intuitiva, carregamento rápido e foco na experiência do usuário (UX)."
+      nome: "WS Burguer",
+      modelo: "E-commerce",
+      imagem: "assets/imagens/logo projetos/WS Burguer.png",
+      descricao: "WS Burguer é uma aplicação de hamburgueria que otimiza a gestão de pedidos usando forEach, map e reduce no JavaScript. O projeto destaca-se pela manipulação dinâmica do cardápio e cálculo de totais em tempo real, focando na experiência do usuário."
     },
     {
-      nome: "Gw-Eletronico",
-      modelo: "E-commerce Vanilla",
-      imagem: "assets/imagens/logo projetos/Logo gw.jpg",
-      descricao: "GW Eletrônico é uma aplicação web moderna e responsiva voltada para o comércio ou gerenciamento de produtos eletrônicos. Desenvolvido inteiramente com tecnologias web fundamentais (HTML, CSS e JavaScript), o projeto destaca-se pela interface intuitiva, carregamento rápido e foco na experiência do usuário (UX)."
+      nome: "WS sorteio",
+      modelo: "arquitetura baseada em eventos",
+      imagem: "assets/imagens/logo projetos/Sorteio de numeros.png",
+      descricao: "WS Sorteios é uma aplicação web de geração numérica automatizada desenvolvida com HTML, CSS e JavaScript. O projeto destaca-se pela distribuição aleatória transparente, processamento instantâneo de dados e foco na usabilidade prática do usuário"
     },
-    {
-      nome: "Gw-Eletronico",
-      modelo: "E-commerce Vanilla",
-      imagem: "assets/imagens/logo projetos/Logo gw.jpg",
-      descricao: "GW Eletrônico é uma aplicação web moderna e responsiva voltada para o comércio ou gerenciamento de produtos eletrônicos. Desenvolvido inteiramente com tecnologias web fundamentais (HTML, CSS e JavaScript), o projeto destaca-se pela interface intuitiva, carregamento rápido e foco na experiência do usuário (UX)."
-    }
+   
+  
   ];
   
  
@@ -53,13 +49,14 @@ const projetos = [
   
         <p class="p-texto-projeto">${item.descricao}</p>
   
-        <a class="a-projeto" id="a-projeto1" href="#">Webflow</a>
+        <a class="a-projeto" id="a-projeto1" href="#">VS code</a>
         <a class="a-projeto" href="#">Figma</a>
         <a class="a-projeto" href="#">SEO</a>
         <br>
   
-        <button class="bnt-projeto">GitHub</button>
-        <button class="bnt-projeto2">Ver Online</button>
+        <a href="https://github.com/W-S-WesleySantana" target="_blank" rel="noopener noreferrer" class="bnt-projeto">GitHub</a>
+
+        <a href="https://w-s-wesleysantana.github.io/Eletronicos-GW/" target="_blank" rel="noopener noreferrer" class="bnt-projeto2">Ver Online</a>
       `;
   
       listaHTML.appendChild(li);
